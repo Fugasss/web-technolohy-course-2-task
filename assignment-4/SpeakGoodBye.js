8ec8e705ede026ad9d61b59d720f874676f3d691
Fugasss <krip.1.101@gmail.com>
2024-11-15 22:46:00 +0500

    Assignment-4

---
+++ assignment-4/SpeakGoodBye.js
@@ -0,0 +1,12 @@
+(function(window) {
+
+  let speakWord = "Good Bye";
+
+  let byeSpeaker = {};
+  
+  byeSpeaker.speak = function(name) {
+    console.log(speakWord + " " + name);
+  }
+
+  window.byeSpeaker = byeSpeaker;
+})(window); 
