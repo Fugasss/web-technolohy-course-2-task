77c5bba9d30f0630c9caeb3e83dfee9d4b92eb43
Fugasss <krip.1.101@gmail.com>
2024-11-20 11:45:30 +0500

    upd

--- project/js/homepage.js
+++ project/js/homepage.js
@@ -1,49 +1,91 @@
 (function (window) {
-    const ITEMS_LIST_TEMPLATE_URL = "templates/homepage/home_items_list_template.html";
-    const ITEM_TEMPLATE_URL = "templates/homepage/home_item_template.html";
-  
-    const ALL_GAMES_DATA_URL = "data/items.json"
+    const ALL_GAMES_DATA_URL = "data/items.json";
     
-    const MAX_DESCRIPTION_LENGTH = 230;
-  
-    document.addEventListener("DOMContentLoaded", function (event) {
-      showLoading("#main-content");
-      $ajaxUtils.get(
-        ALL_GAMES_DATA_URL,
-        showHomeHtml,
-        true);
-    });
-  
-    function showHomeHtml(games) {
-      $ajaxUtils.get(
-        ITEMS_LIST_TEMPLATE_URL,
-        function (containerTemplate) {
-          $ajaxUtils.get(
-            ITEM_TEMPLATE_URL,
-            function (itemTemplate) {
+    async function loadGamesById(gamesId){
+      const result = [];
 
-              let container = containerTemplate;
-              let result = "";
+      for (const id of gamesId) {
+        result.push((await $ajaxUtils.fetch(`data/games/${id}.json`, true)));
+      }
+
+      return result;
+    }
+
+    function generateContent(games, itemTemplate){
+      const MAX_DESCRIPTION_LENGTH = 230;
+
+      let result = "";
               
-              for (const game of games) {
-                result += insertProperties(itemTemplate, {
-                    "item_id": game.id,
-                    "item_image": game.img,
-                    "item_name": game.name,
-                    "item_short_description": (game.short_description.length > MAX_DESCRIPTION_LENGTH ? game.short_description.slice(0, MAX_DESCRIPTION_LENGTH) + "..." : game.short_description),
-                    "item_price": format_currency(game.price)
-                });
-              }
-  
-              container = insertProperty(container, "content", result);
-  
-              insertHtml("#main-content", container);
-  
-            },
-            false
-          )
-        },
-        false);
+      for (const game of games) {
+        result += insertProperties(itemTemplate, {
+            "item_id": game.id,
+            "item_image": game.img,
+            "item_name": game.name,
+            "item_short_description": (game.short_description.length > MAX_DESCRIPTION_LENGTH ? game.short_description.slice(0, MAX_DESCRIPTION_LENGTH) + "..." : game.short_description),
+            "item_price": format_currency(game.price)
+        });
+      }
+
+      return result;
+    }
+
+    async function generatePopular(popularGames){
+      const containerTemplate = await $ajaxUtils.fetch("templates/homepage/popular/carousel_template.html", false);
+      const imageTemplate = await $ajaxUtils.fetch("templates/homepage/popular/image_template.html", false);
+      const indicatorTemplate = await $ajaxUtils.fetch("templates/homepage/popular/indicator_template.html", false);
+      
+      const getRandomImage = function(game){
+        const images = game.imgs;
+
+        if(images.length == 0){
+          return null;
+        }
+        if(images.length == 1){
+          return images[0];
+        }
+
+        return images.slice(1)[Math.floor(Math.random() * (images.length - 1))];
+      }
+
+      let images = "";
+      let indicators = "";
+
+      for (const ind in popularGames) {
+        images += insertProperties(imageTemplate, {
+          "image_url": "images/" + getRandomImage(popularGames[ind]),
+          "interval": (ind == 0 ? 5000 : 3000),
+          "active_or_empty":(ind == 0 ? "active" : "" ),
+          "caption": popularGames[ind].name
+        });
+
+        indicators += insertProperties(indicatorTemplate, {
+          "id": ind,
+          "label": "Slide " + ind,
+          "active_or_empty": (ind == 0 ? "active" : "" ),
+          "current": (ind == 0)
+        });
+
+      }
+
+      return insertProperties(containerTemplate, {
+        "indicators": indicators,
+        "images":images
+      });
+    }
+
+    async function showHomeHtml(games) {
+      const containerTemplate = await $ajaxUtils.fetch("templates/homepage/home_items_list_template.html", false);
+      const itemTemplate      = await $ajaxUtils.fetch("templates/homepage/home_item_template.html", false);
+      
+      const popularGamesId = (await $ajaxUtils.fetch("data/popular.json", true)).games_id;    
+      const popularGames = await loadGamesById(popularGamesId);
+
+      let container = insertProperties(containerTemplate, {
+        "content": generateContent(games, itemTemplate),
+        "popular": await generatePopular(popularGames.sort(() => Math.random() - 0.5))
+      });
+      
+      insertHtml("#main-content", container);
     }
 
     window.$page_loader.homeHtml = function (event) {
