2130cb264b5a98a7b12148a32dbe327a05f585e4
Konstantin <krip.1.101@gmail.com>
2024-11-22 11:36:34 +0500

    upd

--- project/js/gamepage.js
+++ project/js/gamepage.js
@@ -1,42 +1,52 @@
 (function (window) {
   const GAME_TEMPLATE_URL = "templates/concreteitem/concrete_item_template.html";
-  const CAROUSEL_IMAGE_TEMPLATE_URL = "templates/concreteitem/carousel_image_template.html"
+  const CAROUSEL_IMAGE_TEMPLATE_URL = "templates/concreteitem/carousel_image_template.html";
+  const RATING_ROW_URL = "templates/star_rating_row_template.html";
+  const STAR_TEMPLATE = "templates/star_template.html";
 
   const GAME_DATA_URL = "data/games/"
 
-  function showConcreteItemHtml(game) {
-    $ajaxUtils.get(
-      GAME_TEMPLATE_URL,
-      function (containerTemplate) {
-        $ajaxUtils.get(
-          CAROUSEL_IMAGE_TEMPLATE_URL,
-          function (carouselImageTemplate) {
-            let container = containerTemplate;
-            let images = "";
+  async function generateRatingHtml(rating){
+    const ratingRow = await $ajaxUtils.fetch(RATING_ROW_URL, false);
+    const starContainer = await $ajaxUtils.fetch(STAR_TEMPLATE, false);
+    const emptyStar = await $ajaxUtils.fetch("images/star_empty.svg", false);
+    const filledStar = await $ajaxUtils.fetch("images/star_filled.svg", false);
 
-            // Insert all images in carousel
-            for (const ind in game.imgs) {
-              images += insertProperties(carouselImageTemplate, {
-                "image_interval": (ind == 0 ? 5000 : 3000),
-                "image_active_or_empty": (ind == 0 ? "active" : ""),
-                "image_source": "images/" + game.imgs[ind]
-              });
-            }
+    let result = "";
+    for(let ind = 0; ind < 5; ind++){
+      result += insertProperty(starContainer, "star", (ind + 1 < rating ? filledStar : emptyStar));
+    }
 
-            container = insertProperties(container, {
-              "carousel_images": images,
-              "game_name": game.name,
-              "game_full_description": game.full_description,
-              "game_rating": game.rating,
-              "game_price": format_currency(game.price)
-            })
+    return insertProperty(ratingRow, "stars", result);
+  }
 
-            insertHtml("#main-content", container);
-          },
-          false
-        )
-      },
-      false);
+  async function showConcreteItemHtml(game) {
+    const containerTemplate = await $ajaxUtils.fetch(GAME_TEMPLATE_URL, false);
+    const carouselImageTemplate = await $ajaxUtils.fetch(CAROUSEL_IMAGE_TEMPLATE_URL, false);
+    
+
+    let container = containerTemplate;
+    let images = "";
+
+    // Insert all images in carousel
+    for (const ind in game.imgs) {
+      images += insertProperties(carouselImageTemplate, {
+        "image_interval": (ind == 0 ? 5000 : 3000),
+        "image_active_or_empty": (ind == 0 ? "active" : ""),
+        "image_source": "images/" + game.imgs[ind]
+      });
+    }
+
+    container = insertProperties(container, {
+      "carousel_images": images,
+      "game_name": game.name,
+      "game_full_description": game.full_description,
+      "game_rating": await generateRatingHtml(game.rating),
+      "game_price": format_currency(game.price)
+    })
+
+    insertHtml("#main-content", container);
+
   }
 
   window.$page_loader.gameHtml = function (game_id) {
